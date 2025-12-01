04635a0b7f631472ccc81102034aad9879251cdc
helio000 <ha2840770@gmail.com>
2025-12-01 05:39:52 -0300

    vercel

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('../src/router.js'); 
+const routes = require('./src/router.js'); 
 
 const app = express();
 
