4e1fd6c21d6b4d4afe3e3c8421fa17f4bd84ece1
Coronel <160642659+helio000@users.noreply.github.com>
2025-12-02 08:57:16 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./src/router.js'); 
+const routes = require('./src/router.js');
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Rotas
 app.use('/', routes);
+
+// Porta para Render / local
+const PORT = process.env.PORT || 3100;
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
 
 module.exports = app;
